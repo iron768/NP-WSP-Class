8d005f62688567ae444ec5cef5e24501262a56ea
CJ <iron768gamer@gmail.com>
2024-03-11 14:39:03 -0400

    Created ProductCard

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { Product } from '@/model/products';
+
+const props = defineProps<{
+    product: Product 
+}>()
+
+const emit = defineEmits<{
+    addToCart: [product: Product]
+}>()
+
+</script>
+
+<template>
+    <div>
+        <div class="card" >
+            <div class="card-image">
+                <img :src="product.thumbnail" :alt="product.title" />
+            </div>
+            <div class="card-content">
+                <p class="price">${{ product.price }}</p>
+                <h3>{{ product.title }}</h3>
+                <i>{{ product.brand }}</i>
+                <p>{{ product.description }}</p>
+                <button @click="emit(`addToCart `, product)" class="button is-primary">Add to Cart</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
